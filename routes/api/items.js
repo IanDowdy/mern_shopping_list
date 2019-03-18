7da5709a5f7705cd97e8d7dd49e31e5a0d8e1dca
Ian <ian_dowdy@outlook.com>
2019-03-18 00:00:29 -0500

    finished initial backend setup

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,13 +13,13 @@ router.get("/", (req, res) => {
     .then(items => res.json(items));
 });
 
-// @route GET api/items
-// @desc  Get All Items
+// @route DELETE api/items/:id
+// @desc  Delete An Item
 // @access Public
-router.get("/", (req, res) => {
-  Item.find()
-    .sort({ date: -1 })
-    .then(items => res.json(items));
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 
 // @route GET api/items
